df7abf62c6094e6f92a985512e73e7b2cae088d1
Helio Kanashiro <helio.fototerra@gmail.com>
2020-10-19 23:16:12 -0300

    Modal com form de cadastro

--- src/components/contacts/ContactModal.jsx
+++ src/components/contacts/ContactModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 
 const customStyles = {
@@ -19,24 +19,78 @@ const customStyles = {
 };
 
 const ContactModal = ({ isOpen, closeModal }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //add localstorage
+    console.log(formData);
+  };
+
+  const checkEmptyForm = () => {
+    return Object.values(formData).every((x) => x === '');
+  };
+
   return (
     <Modal
       id='contact-modal'
-      isOpen={true}
+      isOpen={isOpen}
       onRequestClose={closeModal}
       style={customStyles}>
-      <div className='header'>
-        <p>Criar novo contato</p>
-      </div>
-      <div className='body'>BODY</div>
-      <div className='footer'>
-        <button className='button default' onClick={closeModal}>
-          Cancelar
-        </button>
-        <button className='button success' onClick={closeModal}>
-          Salvar
-        </button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className='header'>
+          <p>Criar novo contato</p>
+        </div>
+        <div className='body'>
+          <div>
+            <label htmlFor='name'>Nome</label>
+            <input
+              name='name'
+              type='text'
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='email'>E-mail</label>
+            <input
+              name='email'
+              type='email'
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='phone'>Telefone</label>
+            <input
+              name='phone'
+              type='text'
+              className='phone'
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className='footer'>
+          <button className='button default' onClick={closeModal}>
+            Cancelar
+          </button>
+          <button
+            type='submit'
+            className={`button ${checkEmptyForm() ? 'disabled' : 'success'}`}
+            disabled={checkEmptyForm()}>
+            Salvar
+          </button>
+        </div>
+      </form>
     </Modal>
   );
 };
