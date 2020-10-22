69f4d8f02d4f82d9b623efd29e7c182fb1af7d6d
Helio Kanashiro <helio.fototerra@gmail.com>
2020-10-22 17:35:32 -0300

    Updates

---
+++ precache-manifest.87d1d5f956986f831fa617b6b9923d13.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ea60db36a4a37c68ffc6e570a9f45c",
+    "url": "/agenda/index.html"
+  },
+  {
+    "revision": "53fbe35eab84b0742e69",
+    "url": "/agenda/static/css/main.1a76c127.chunk.css"
+  },
+  {
+    "revision": "9f801c307b72caeee7bc",
+    "url": "/agenda/static/js/2.3a4cba37.chunk.js"
+  },
+  {
+    "revision": "5f8795b8f28c7d1a90992cb44145be0f",
+    "url": "/agenda/static/js/2.3a4cba37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53fbe35eab84b0742e69",
+    "url": "/agenda/static/js/main.17430070.chunk.js"
+  },
+  {
+    "revision": "3a595f202d0960b425b7",
+    "url": "/agenda/static/js/runtime-main.ac52f630.js"
+  },
+  {
+    "revision": "de526db56e5bbad8bb1b910ac7359949",
+    "url": "/agenda/static/media/ic-book.de526db5.svg"
+  },
+  {
+    "revision": "190ff2d8784528c45f7d147acf6da594",
+    "url": "/agenda/static/media/ic-logo.190ff2d8.svg"
+  },
+  {
+    "revision": "8fd12e49e3c5feb9cf75786d87a82e9c",
+    "url": "/agenda/static/media/ic-search.8fd12e49.svg"
+  }
+]);
